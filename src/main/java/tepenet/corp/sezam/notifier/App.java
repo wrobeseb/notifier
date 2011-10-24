7e0ed3c81f4553804838580a4a6cf83bc1582932
sairo_work <Sebastian.Wrobel3@telekomunikacja.pl>
2011-10-24 22:51:44 +0200

    First commit...

---
+++ src/main/java/tepenet/corp/sezam/notifier/App.java
@@ -0,0 +1,13 @@
+package tepenet.corp.sezam.notifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
